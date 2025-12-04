ed212055efade3f6e58cc21daf9a1da225ac3c15
Mo77amed7 <muhamedrashad99@gmail.com>
2025-12-04 18:22:56 +0200

    feat: add initial component structure for Balance, Header, and other components

---
+++ src/components/Balance.vue
@@ -0,0 +1,13 @@
+<template>
+  <section>
+    <p>Your Balance</p>
+    <p>$0.00</p>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Balance",
+};
+</script>
+<style scoped></style>
---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <header>
+    <h2>Expense Tracker</h2>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style scoped></style>
